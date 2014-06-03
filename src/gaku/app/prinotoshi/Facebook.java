15c62f4626687085adc99d4dafbd6200e820aa53
rui3718 <rui3718@gmail.com>
2014-06-03 21:49:28 +0900

    Facebook

--- src/gaku/app/prinotoshi/Facebook.java
+++ src/gaku/app/prinotoshi/Facebook.java
@@ -12,7 +12,7 @@ public class Facebook extends Activity {
 	
 	public WebView myWebView;
 	String URL = "http://pokipurin.info/login.php";
-	//String redirect_uri = "http://pokipurin.info/log.php?";
+	String scheme = "app://";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState){
@@ -21,29 +21,25 @@ public class Facebook extends Activity {
 		
 		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
 		String score = pref.getString("score", "0");
+		String URL = "http://pokipurin.info/login.php?score=" + score;
 		
 		
 		myWebView = (WebView)findViewById(R.id.webView1);
-		myWebView.setWebViewClient(new WebViewClient()/*{
+		myWebView.setWebViewClient(new WebViewClient(){
 			@Override
 			public boolean shouldOverrideUrlLoading(WebView view_, String url_){
 				super.shouldOverrideUrlLoading(view_, url_);
-				if(url_.startsWith(redirect_uri)){
-					String[] params = url_.substring(redirect_uri.length(),url_.length()).split("&");
-					String code = (params[0].split("="))[1];
-					String state = (params[1].split("="))[1];
-					Log.v("URL:",url_);
-					Log.v("CODE:",code);
-					Log.v("STATE:",state);
-					String URL = "http://pokipurin.info/log.php?";
-							
+				if(url_.startsWith(scheme)){
+					String method = url_.substring(scheme.length(),url_.length());
 					
-					myWebView.loadUrl(URL);
+					if(method.equals("end")){
+						finish();
+					}
 					return false;
 				}
 				return super.shouldOverrideUrlLoading(view_, url_);
 			}
-		}*/);
+		});
 		myWebView.getSettings().setDefaultTextEncodingName("Shift-JIS");
 		myWebView.getSettings().setJavaScriptEnabled(true);
 		myWebView.loadUrl(URL);	
